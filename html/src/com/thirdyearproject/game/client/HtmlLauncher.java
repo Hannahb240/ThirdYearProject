f6e3844f63af92933ee5038c3f7276195ca67f78
[Hannah Berrisford] <[Hannah.Berrisford@student.manchester.ac.uk]>
2017-09-27 13:40:55 +0100

    Initial commit

---
+++ html/src/com/thirdyearproject/game/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.thirdyearproject.game.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.thirdyearproject.game.ThirdYearProject;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new ThirdYearProject();
+        }
+}
